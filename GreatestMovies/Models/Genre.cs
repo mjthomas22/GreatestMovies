4ab852c64e7f49b9a25be62c89f2935fcffede38
mjthomas22 <mthomas22@gmail.com>
2017-07-26 15:18:11 -0400

    add classes

---
+++ GreatestMovies/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreatestMovies.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        public string GenreType { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
